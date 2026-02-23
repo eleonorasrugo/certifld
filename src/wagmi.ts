1ab6f42e3d6456f9a7efbcae8f5e10df7d34ca6c
eleonorasrugo <eleonorasrugo937@gmail.com>
2026-02-23 17:16:49 +0000

    fix

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,16 +1,33 @@
 import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
-import { mainnet, sepolia, polygon, bsc, arbitrum, optimism } from "wagmi/chains";
+import {
+  mainnet,
+  sepolia,
+  polygon,
+  bsc,
+  arbitrum,
+  optimism,
+} from "wagmi/chains";
 
 const projectId = import.meta.env.VITE_WALLETCONNECT_PROJECT_ID;
 
 const metadata = {
   name: "Wallet Scanning",
   description: "Decentralized Wallet Protection",
-  url: window.location.origin,
+  url:
+    typeof window !== "undefined"
+      ? window.location.origin
+      : "https://localhost",
   icons: ["https://avatars.githubusercontent.com/u/37784886"],
 };
 
-export const chains = [mainnet, sepolia, polygon, bsc, arbitrum, optimism] as const;
+export const chains = [
+  mainnet,
+  sepolia,
+  polygon,
+  bsc,
+  arbitrum,
+  optimism,
+] as const;
 
 export const config = defaultWagmiConfig({
   chains,
